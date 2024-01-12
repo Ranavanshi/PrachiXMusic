ff2452fa4a56f4572f91557439552609a5471408
Ranavanshi <dkrana41118@gmail.com>
2024-01-12 20:28:02 +0530

    prachi

---
+++ PrachiMusic/utilities/events/filters.py
@@ -0,0 +1,9 @@
+# Powered By @noob_savvy
+from typing import Union, List
+from pyrogram import filters
+from PrachiMusic.utilities.config import COMMAND_PREFIXES
+
+
+# @THE_WEBNET_NETWORK
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
